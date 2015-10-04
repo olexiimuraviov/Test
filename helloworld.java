446afa53ffab01286b4d41dfccd3620e56782b60
olexiimuraviov <omuraviov.dev@gmail.com>
2015-10-04 11:00:22 +0300

    change helloworld

--- helloworld.java
+++ helloworld.java
@@ -2,5 +2,9 @@
 class main {
 	void static main(String[] argv) {
 		System.out.println("Hello World!");
+		//add some new stuff to code
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
 	}
 }
